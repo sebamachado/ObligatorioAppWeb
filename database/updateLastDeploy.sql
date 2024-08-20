0babaf61f92d7ab5cb8ac31dd21725127f0bf409
sebamachado <138078030+sebamachado@users.noreply.github.com>
2024-08-20 15:58:07 -0300

    Update updateLastDeploy.sql

--- database/updateLastDeploy.sql
+++ database/updateLastDeploy.sql
@@ -1,3 +1,7 @@
+-- Obtén la fecha y hora actual en UTC
+DECLARE @CurrentUTCDateTime DATETIME = GETUTCDATE();
+
+-- Ajusta la fecha y hora actual al huso horario de Montevideo (UTC-3) y actualiza la columna
 UPDATE parametros
-SET valor = FORMAT(SWITCHOFFSET(CONVERT(DATETIMEOFFSET, GETDATE()) AT TIME ZONE 'UTC', '-03:00'), 'dd/MM/yyyy HH:mm:ss')
+SET valor = FORMAT(SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @CurrentUTCDateTime), '-03:00'), 'dd/MM/yyyy HH:mm:ss')
 WHERE nombre = 'ultimoDeploy';
