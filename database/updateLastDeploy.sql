eeaf8113146e613a90bae7769583c7c452f7763c
sebamachado <smachadoramos2001@gmail.com>
2024-08-22 22:35:08 -0300

    Delete updateLastDeploy.sql

--- database/updateLastDeploy.sql
+++
@@ -1,7 +0,0 @@
--- Obtén la fecha y hora actual en UTC
-DECLARE @CurrentUTCDateTime DATETIME = GETUTCDATE();
-
--- Ajusta la fecha y hora actual al huso horario de Montevideo (UTC-3) y actualiza la columna
-UPDATE parametros
-SET valor = FORMAT(SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @CurrentUTCDateTime), '-03:00'), 'dd/MM/yyyy HH:mm:ss')
-WHERE nombre = 'ultimoDeploy';
